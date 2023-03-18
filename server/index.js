5c08f72cf861723dddbb7ba6e840be7d7d13c12a
Christopher Hage <hage.chris@yahoo.com>
2023-03-17 22:00:00 -0300

    food api set up on server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import axios from "axios";
 import "dotenv/config";
 
 const port = 5000 || process.env.PORT;
@@ -7,8 +8,19 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/api", async (req, res) => {
-  res.send("Hello");
+app.get("/api/:type/:method", async (req, res) => {
+  const { type, method } = req.params;
+
+  try {
+    const response = await axios.get(
+      `https://api.spoonacular.com/${type}/${method}?apiKey=${process.env.apiKey}`,
+      { params: req.query }
+    );
+
+    res.send(response.data);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
 });
 
 const start = () => {
