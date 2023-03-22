ad12ce08f355d807e503ea778e3516f9c8d5d39e
Christopher Hage <hage.chris@yahoo.com>
2023-03-21 21:43:17 -0300

    navbar added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,7 +5,25 @@ import "../styles/Navbar.css";
 const Navbar = () => {
   return (
     <>
-      <div className="nav-container">Navbar</div>
+      <div className="nav">
+        <div className="nav-container">
+          <div className="nav-logo">
+            <img src="/AppLogo.png" alt="" />
+          </div>
+          <div className="nav-links">
+            <div className="nav-link">Home</div>
+            <div className="nav-link">About</div>
+            <div className="nav-link">Credits</div>
+          </div>
+          <div className="nav-register">
+            <div className="nav-btns">
+              <div className="login-btn">Log in</div>
+              <div className="signup-btn">Sign up</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <Outlet />
     </>
   );
